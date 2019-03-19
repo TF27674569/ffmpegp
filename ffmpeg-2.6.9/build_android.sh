69e0c1878b5eef1ea3ef5fd8395b874e0113f2c1
阳阳阳 <yangyangyang@yangyangyangdeMacBook-Pro.local>
2019-03-19 16:38:06 +0800

    first commit

---
+++ ffmpeg-2.6.9/build_android.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+make clean
+export NDK=/Users/yangyangyang/Downloads/android-ndk-r14b
+export SYSROOT=$NDK/platforms/android-19/arch-arm/
+export TOOLCHAIN=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64
+export CPU=arm
+export PREFIX=$(pwd)/android/$CPU
+export ADDI_CFLAGS="-marm"
+
+./configure --target-os=linux \
+--prefix=$PREFIX --arch=arm \
+--disable-doc \
+--enable-shared \
+--disable-static \
+--disable-yasm \
+--disable-symver \
+--enable-gpl \
+--disable-ffmpeg \
+--disable-ffplay \
+--disable-ffprobe \
+--disable-ffserver \
+--disable-doc \
+--disable-symver \
+--cross-prefix=$TOOLCHAIN/bin/arm-linux-androideabi- \
+--enable-cross-compile \
+--sysroot=$SYSROOT \
+--extra-cflags="-Os -fpic $ADDI_CFLAGS" \
+--extra-ldflags="$ADDI_LDFLAGS" \
+$ADDITIONAL_CONFIGURE_FLAG
+make clean
+make -j12
+make install
